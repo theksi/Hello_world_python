88fb2e49a8216581176eead1b5424dbb60615566
Frédéric <43031034+theksi@users.noreply.github.com>
2019-02-10 18:02:11 +0100

    added test on variables

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 print ("Hello World")
 print ("Encore une fois")
+greeting = "Hello World"
+print(greeting)
+greeting = greeting.split(" ")[0]
+print (greeting)
+print (greeting + " Nick") 
+number = 1
+secondnumber = 2
+print(number * secondnumber + secondnumber*number)
+check = True
