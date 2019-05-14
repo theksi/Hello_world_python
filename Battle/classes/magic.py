0d8f124346d00c108811b90f8cf585330b89ef53
Meulenijzer Frédéric <EFC456@engie.com>
2019-05-14 14:52:09 +0200

    Introduced Magic class

---
+++ Battle/classes/magic.py
@@ -0,0 +1,7 @@
+class Spell:
+    def __init__(self, name, cost, dmg, type):
+        self.name = name
+        self.cost = cost
+        self.dmg = dmg
+        self.type = type
+#   Self refer to this insance of the class
